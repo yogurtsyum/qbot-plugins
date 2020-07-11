c83ba3eface397c2d943a6bb1a0ae5c8bbed1879
zachariapopcorn <52849920+zachariapopcorn@users.noreply.github.com>
2020-07-10 17:01:01 -0700

    Create setprefix.js

---
+++ setprefix_plugin/setprefix.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+exports.run = (client, message, args) => {
+    if(message.author.id != "YOUR DISCORD ID HERE") {
+        return message.channel.send("You don't have permission to run this command!");
+    }
+    let prefix = args[0];
+    if(!prefix) {
+        return message.channel.send("Please enter a new prefix!");
+    }
+    process.env.prefix = prefix;
+    return message.channel.send("Successfully changed the prefix to " + prefix + ". This will be reverted back to the prefix set in the env file when the bot restarts.");
+}
