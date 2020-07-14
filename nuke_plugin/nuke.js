f2ceda34cb1b168a1208711f1525d709dc83ab84
zachariapopcorn <52849920+zachariapopcorn@users.noreply.github.com>
2020-07-14 09:59:55 -0700

    Update nuke.js

--- nuke_plugin/nuke.js
+++ nuke_plugin/nuke.js
@@ -2,6 +2,9 @@ exports.run = async (client, message, args) => {
     if(!message.channel.deletable) {
         return message.channel.send("I don't have permission to run this command! Please ensure that I have the 'Manage Channels' permission!");
     }
+    if(!message.member.hasPermission('MANAGE_CHANNELS')) {
+        return message.channel.send("You don't have permission to run this command");
+    }
     message.channel.delete();
     let clone = await message.channel.clone();
     return clone.send('Nuked channel!');
