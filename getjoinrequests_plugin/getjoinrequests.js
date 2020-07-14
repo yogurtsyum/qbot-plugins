fd4aacd9780c2a65a87cd5abc7b8c9f34f3012fc
zachariapopcorn <52849920+zachariapopcorn@users.noreply.github.com>
2020-07-14 10:31:08 -0700

    Update getjoinrequests.js

--- getjoinrequests_plugin/getjoinrequests.js
+++ getjoinrequests_plugin/getjoinrequests.js
@@ -2,6 +2,9 @@ const roblox = require('noblox.js');
 require('dotenv').config();
 
 exports.run = async (client, message, args) => {
+    if(!message.member.roles.cache.some(role => ['Get Join Logs Permission'].includes(role.name))) {
+        return message.channel.send("I'm sorry, but you don't have permission to run this command. Please ensure you have the 'Get Join Logs Permission' role");
+    }
     let groupid = Number(process.env.groupId);
     try {
         let joins = await roblox.getJoinRequests(groupid);
