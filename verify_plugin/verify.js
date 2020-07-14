986756fc760e9e29a7091d7381e990cb25ffc871
zachariapopcorn <52849920+zachariapopcorn@users.noreply.github.com>
2020-07-14 10:43:42 -0700

    Update verify.js

--- verify_plugin/verify.js
+++ verify_plugin/verify.js
@@ -36,20 +36,12 @@ exports.run = async (client, message, args) => {
         }
         return message.channel.send(`Success! You have verified as ${username}!`);
     }
-    const verifyStatus = "dog cat turtle woof cup tea bottle";
-    message.channel.send(`Please put this as your status: '${verifyStatus}' and type 'ready' when done.`);
-    const determine2 = await message.channel.awaitMessages(filter, {max: 1});
-    const answer2 = determine2.first().content;
-    if(answer2 != "ready") {
-        return message.channel.send("I'm sorry, but you didn't answer 'ready', this has been cancelled.");
-    }
-    if(await roblox.getStatus(id) != verifyStatus) {
-        return message.channel.send(`You didn't change your status to '${verifyStatus}'! This has been cancelled!`);
-    }
+    if(!message.member.bannable) return;
+    if(!message.guild.me.hasPermission('BAN_MEMBERS')) return;
     try {
-        await db.set(message.guild.id + ":" + message.author.id + "-username", username);
-    } catch (err) {
-        return message.channel.send("There was an error while saving data: " + err);
+        message.author.send('AUTOMATIC SYSTEM: You have been banned from this server since you are now under the accusation that you are underage for Discord')
+    } catch {
+        console.log('Couldnt send a automatic system message');
     }
-    return message.channel.send(`Success! You have verified as ${username}!`);
+    return message.member.ban({reason: 'Under the accusation of being underage for Discord'});
 }
