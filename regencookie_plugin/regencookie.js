5d9f0ab8ec657d9351acf417a34fc9f988202eac
zachariapopcorn <52849920+zachariapopcorn@users.noreply.github.com>
2020-08-03 11:05:59 -0700

    Update regencookie.js

--- regencookie_plugin/regencookie.js
+++ regencookie_plugin/regencookie.js
@@ -1,26 +1,28 @@
 const roblox = require('noblox.js');
 const fs = require('fs');
+const envfile = require('envfile');
+const sourcePath = '.env';
 require('dotenv').config();
 
 exports.run = async (client, message, args) => {
-    if(message.author.id != "DISCORD ID HERE") {
+    if(message.author.id !== "DISCORD ID HERE") {
         return message.channel.send("You don't have permission to run this command!");
     }
     if(message.channel.type !== "dm") {
         return message.channel.send("This command only works in DMS!");
     }
-    let cookie = process.env.cookie;
     let isValidCookie = true;
     try {
-        await roblox.setCookie(cookie);
+        await roblox.follow(1);
     } catch {
         isValidCookie = false;
     }
     if(isValidCookie == true) {
+        await roblox.unfollow(1);
         return message.channel.send("The cookie that is currently being used is a valid one!");
     }
     if(!args[0]) {
-        return message.client.send("Please insert a username!");
+        return message.channel.send("Please insert a username!");
     }
     try {
         await roblox.getIdFromUsername(args[0]);
@@ -38,7 +40,17 @@ exports.run = async (client, message, args) => {
     } catch (err) {
         return message.channel.send("There was an error while getting a new cookie: " + err);
     }
+    let parsedFile = envfile.parse(sourcePath);
+    parsedFile.token = process.env.token;
+    parsedFile.prefix = process.env.prefix;
+    parsedFile.cookie = jar.session;
+    parsedFile.groupId = process.env.groupId;
+    parsedFile.maximumRank = process.env.maximumRank;
+    parsedFile.logchannelid = process.env.logchannelid;
+    parsedFile.shoutchannelid = process.env.shoutchannelid;
+    process.env.shoutchannelid;
+    fs.writeFileSync('./.env', envfile.stringify(parsedFile));
     process.env.cookie = jar.session;
     roblox.setCookie(jar.session);
-    return message.author.send("I have set the session's cookie to the working cookie, which means that the env file cookie is still the same. If you want to update that, please set the cookie value to " + jar.session);
+    return message.channel.send("I have regenerated the cookie!");
 }
