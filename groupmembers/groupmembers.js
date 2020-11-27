ae90a6caa28065c5b3acb76ac9c236e6043b2f63
nathan-dev27 <63113750+nathan-dev27@users.noreply.github.com>
2020-11-27 17:24:34 +0800

    Update groupmembers.js

--- groupmembers/groupmembers.js
+++ groupmembers/groupmembers.js
@@ -1,27 +1,14 @@
 require('dotenv').config();
+onst fetch = require("node-fetch")
 
 exports.run = async (client, message, args) => {
-  const fetch = require("node-fetch")
-  var request = require("request")
-  function getData(field) {
-    return new Promise((resolve, reject) => {
-      request(
-        { uri: `https://groups.roblox.com/v1/groups/${process.env.groupId}` },
-        function (error, response, body) {
-          if (response.statusCode == 200) resolve(JSON.parse(body)[field])
-          else reject("Something went wrong!")
-        }
-      );
-    });
-  }
-  getData("memberCount").then((groupMembers) => message.channel.send({
-    embed: {
-      description: `The group has  **` + groupMembers + `** members!`,
-      color: `RANDOM`,
-      author: {
-        name: message.author.tag,
-        icon_url: message.author.displayAvatarURL()
-      }
+  const { memberCount } = await fetch(`https://groups.roblox.com/v1/groups/7101928`).then(response => response.json());
+  message.channel.send({embed: {
+    color: "RANDOM",
+    description: `There are ${memberCount} group members!`,
+    author: {
+      name: message.author.tag,
+      icon_url: message.author.displayAvatarURL()
     }
-  }))
+  }})
 };
