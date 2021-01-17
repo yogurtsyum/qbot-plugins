7e28d8d2aa176f7a5669251a2ffebce034eeb07f
Brandon Saraceno <brandoge91@gmail.com>
2021-01-17 15:49:55 -0500

    Update setsessionchannel.js

--- session_plugin/setsessionchannel.js
+++ session_plugin/setsessionchannel.js
@@ -1,6 +1,19 @@
 const db = require('../db.js');
+const path = require('path')
+const Discord = require('discord.js')
 
-exports.run = async (client, message, args) => {
+const config = {
+    description: 'sets session channel',
+    aliases: [],
+    usage: '[channel]',
+    rolesRequired: [],
+    category: 'roblox'
+}
+
+
+module.exports = {
+    config,
+    run: async (client, message, args) => {
     if(!args[0]) {
         return message.channel.send("Please mention a channel that is going to be used for a session channel!");
     }
@@ -11,7 +24,9 @@ exports.run = async (client, message, args) => {
     try {
         await db.set("sessionchannel", channel.id);
     } catch (err) {
-        return message.channel.send("There was an error while saving the session channel to the database: " + err);
+        return message.channel.send("There was an error while saving the session channel to the database:\n " + err);
     }
     return message.channel.send("Successfully set the session channel to <#" + channel.id + ">!");
+
+    }
 }
