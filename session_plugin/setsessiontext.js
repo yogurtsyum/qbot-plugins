2890c59e185f8b82e352d6c912b014b77dd67668
Brandon Saraceno <brandoge91@gmail.com>
2021-01-17 15:52:30 -0500

    Update setsessiontext.js

--- session_plugin/setsessiontext.js
+++ session_plugin/setsessiontext.js
@@ -1,14 +1,24 @@
 const db = require('../db.js');
+const path = require('path')
+const Discord = require('discord.js')
+                        
+const config = {
+    description: 'Sets session text',
+    aliases: [],
+    usage: '[text]',
+    rolesRequired: [MANAGE_MESSAGES],
+    category: 'roblox'
+}
 
-exports.run = async (client, message, args) => {
+
+module.exports = {
+    config,
+    run: async (client, message, args) => {
   const message1 = args.join(' ')
   if (!message1) {
     message.channel.send('Please supply a session argument!')
   }
-
-  if (message1) {
   message.channel.send('Great! Set the session message to **' + message1 + "**!")
   await db.set("sessiontext", message1);
-  console.log("set the session msg to" + message1)
-  }
+    }
 }
