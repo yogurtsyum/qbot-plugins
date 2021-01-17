02313933e88976a12bf4bcb3017189087d7bad8b
Brandon Saraceno <brandoge91@gmail.com>
2021-01-17 15:47:14 -0500

    Update session.js

--- session_plugin/session.js
+++ session_plugin/session.js
@@ -1,11 +1,22 @@
 const discord = require("discord.js");
-const request = require("request");
-const chalk = require('chalk')
+const path = require('path')
+const db = require("../db.js")
 
-exports.run = async (client, message, args, db) => {
+const config = {
+    description: 'starts a session',
+    aliases: [],
+    usage: '',
+    rolesRequired: ['MANAGE_MESSAGES'],
+    category: 'Roblox'
+}
+
+module.exports = {
+    config,
+    run: async (client, message, args) => {
   const sessiontext = await db.get('sessiontext')
-  console.log(sessiontext)
   const channelId = await db.get('sessionchannel')
+  if (sessiontext === null) return
+  if (channelId === null) return
       let channel = message.guild.channels.cache.find(channel => channel.id === channelId);
   const msg = await channel.send(sessiontext)
   const successembed = new discord.MessageEmbed()
@@ -13,6 +24,7 @@ exports.run = async (client, message, args, db) => {
     .setDescription('Started session successfully!')
     .setTimestamp()
     .setColor("GREEN")
-    .setFooter('lua#2400')
+    .setFooter('qbot')
   message.channel.send(successembed)
+    }
 };
